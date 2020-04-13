68c7cebe069efdcc632305fd1bebb6112b36fbf6
sgan8 <sgan8@gatech.edu>
2020-04-13 18:26:24 -0400

    correct reference to templates/ and templates/static/ folder

--- webserver.py
+++ webserver.py
@@ -1,16 +1,8 @@
 from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template("home.html")
-
-@app.route("/team")
-def team():
-    return render_template("team.html")
-
-@app.route("/analyze")
-def analyze():
-    return render_template("analyze.html")
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='templates/'
+            )
 
 
 if __name__=="__main__":
